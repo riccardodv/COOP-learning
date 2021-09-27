04550a454aebecfaacf199b42dda95887805bdf5
Riccardo Della Vecchia <riccardo.dellavecchia91@gmail.com>
2021-09-27 16:18:22 +0200

    fix plots

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ def run_algo(ban, coop, T):
         coop.act()
         r = np.append(r, ban.total_regret())
     ########## SOME PRINT ######################################################
-        # print("----> Round =", t+1)
-        # print("eta = ", coop.eta)
-        # print("total regret:", ban.total_regret())
-        # print("EDGES:", ban.net_agents.edges)
-        # print("optima arm:", arms_means[0])
-        # for v in ban.net_agents.nodes:
-        #     print("agent", v, "has neighbours:",[u for u in ban.net_agents.neighbors(v)],
-        #     "T:", ban.net_agents.nodes[v]['T'], "r:",np.dot(ban.subopt, ban.net_agents.nodes[v]['T']), "Tsub:", ban.t - ban.net_agents.nodes[v]['T'][0])
+        print("----> Round =", t+1)
+        print("eta = ", coop.eta)
+        print("total regret:", ban.total_regret())
+        print("EDGES:", ban.net_agents.edges)
+        print("optima arm:", arms_means[0])
+        for v in ban.net_agents.nodes:
+            print("agent", v, "has neighbours:",[u for u in ban.net_agents.neighbors(v)],
+            "T:", ban.net_agents.nodes[v]['T'], "r:",np.dot(ban.subopt, ban.net_agents.nodes[v]['T']), "Tsub:", ban.t - ban.net_agents.nodes[v]['T'][0])
     ############################################################################
     return ban, coop, r/coop.Q
 
@@ -71,7 +71,7 @@ T = 10
 prob_ER_a = 0.3; prob_ER_f = 0.2
 arms_means = 1/2 * np.ones(num_arms)
 arms_means[0] = 0.1 #1/2 - np.sqrt(num_arms/T)
-sample = 1000
+sample = 10
 
 def run_indep_coop(arms_means, num_agents, num_arms, n, f, prob_ER_a, prob_ER_f, T):
 # rr , rr_indepp = [], []
@@ -101,7 +101,7 @@ if __name__ == "__main__":
     errors_indepp = np.std(r_indepp, axis=0)
     x = [i for i in range(T)]
     # bound = UB(x, n+f, num_arms, coop.alpha_feed, coop.alpha_agents)
-
+    pp.figure(figsize=(10, 6))
     pp.plot(x, means, label = "COOPalgo", color='blue')
     pp.fill_between(x, means - errors,means + errors, color='blue', alpha=0.2)
     pp.plot(x, means_indepp, label = "independent", color='red')
@@ -111,5 +111,5 @@ if __name__ == "__main__":
     tit += f", q={q}, samples={sample}, ER_a={prob_ER_a}, ER_f={prob_ER_f}"
     pp.title(tit)
     pp.legend()
-    pp.savefig('tit.eps')
+    pp.savefig('tit.pdf', dpi=200)
     pp.show()
