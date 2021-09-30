a18493c12c0d68d12a72d1f4c74f48c2aec1b393
Riccardo Della Vecchia <riccardo.dellavecchia91@gmail.com>
2021-09-30 18:48:00 +0200

    delete restart functions and other simplifications

--- bandit.py
+++ bandit.py
@@ -38,10 +38,11 @@ class Bandit:
     return np.dot(self.subopt, self.net_agents.nodes[v]['T'])
 
   def total_regret(self):
-    R = 0
+    R = 0; Q = 0
     for v in self.net_agents.nodes:
+      Q += self.net_agents.nodes[v]['q']
       R += self.regret(v)
-    return R
+    return R/Q
 
   def activate_players(self):
     self.activations = []
@@ -50,8 +51,8 @@ class Bandit:
 
   def play(self, arms):
     assert len(self.net_agents.nodes) == len(arms)
-    # Compute rewards for this round
-    rewards = np.random.binomial(1, self.means)
+    # Compute losses for this round
+    losses = np.random.binomial(1, self.means)
     self.t += 1
     # delete previous losses and prepare for new onens
     for v in self.net_agents.nodes:
@@ -61,32 +62,20 @@ class Bandit:
     for v in gen:
       message = self.net_agents.nodes[v]['m'] # messages ~ probabilities
       i = arms[v]
-      reward = rewards[i]
+      loss = losses[i]
       self.net_agents.nodes[v]['T'][i] += 1
-      self.net_agents.nodes[v]['S'][i] += reward
+      self.net_agents.nodes[v]['S'][i] += loss
       neigh_feed = [j for j in nx.single_source_shortest_path_length(self.net_feed, i, self.f).keys()]
       for j in neigh_feed:
-        self.net_agents.nodes[v]['new_ls'].append((self.t, v, j, rewards[j], message[j]))
+        self.net_agents.nodes[v]['new_ls'].append((self.t, v, j, losses[j], message[j]))
     # Update for all the messages coming from active agents:
     for v in self.net_agents.nodes:
       neigh_agents = [u for u in nx.single_source_shortest_path_length(self.net_agents, v, self.n).keys()]
-      # print("neighbour of", v, "has elements", neigh_agents)
       for u in neigh_agents:
         if u != v:
           self.net_agents.nodes[v]['new_ls'] += self.net_agents.nodes[u]['new_ls']
     return 0
 
-  def restart_bandit(self, prepare_for_indep = True):
-    self.t = 0
-    if prepare_for_indep:
-      self.f = 0
-      self.n = 0
-    for v in self.net_agents.nodes:
-        self.net_agents.nodes[v]['new_ls'] = []
-        self.net_agents.nodes[v]['m'] = np.zeros(self.arms())
-        self.net_agents.nodes[v]['T'] = np.zeros(self.arms())
-        self.net_agents.nodes[v]['S'] = np.zeros(self.arms())
-        self.activations = []
 
 class COOP_algo():
   def __init__(self, bandit, T=100):
@@ -108,7 +97,6 @@ class COOP_algo():
         self.W[i,v] = self.W[i,v] * np.exp(-eta*self.ev_lhat(i,v))
     for v in range(len(self.bandit.net_agents.nodes)):
       self.P[:,v] = self.W[:,v]/np.linalg.norm(self.W[:,v], ord=1)
-      # print("P = ", self.P[:,v])
 
   def update_buffer_and_predict(self):
     # Adaptive learning rate:
@@ -116,9 +104,7 @@ class COOP_algo():
     self.update(self.eta)
     # Fixed learning rate:
     # self.update(self.eta)
-    # print("eta = ", self.eta)
     self.buffer.append(copy.copy(self.P))
-    # print("self.buffer: ", self.buffer)
     if self.bandit.t >= self.bandit.f + self.bandit.n:
       return self.buffer.popleft()
     else:
@@ -127,8 +113,8 @@ class COOP_algo():
   def ev_lhat(self, i, v, epsilon = 10**-20):
     new_losses = self.bandit.net_agents.nodes[v]['new_ls']
     if len(new_losses) != 0:
-      loss_comps = {(j, reward) for s, u, j, reward, prob in new_losses} # maybe add prob????
-      seen_comps = {j for s, u, j, reward, prob in new_losses}
+      loss_comps = {(j, loss) for s, u, j, loss, prob in new_losses} # maybe add prob????
+      seen_comps = {j for s, u, j, loss, prob in new_losses}
       neigh_agents = [u for u in nx.single_source_shortest_path_length(self.bandit.net_agents, v, self.bandit.n).keys()]
       neigh_feed = [j for j in nx.single_source_shortest_path_length(self.bandit.net_feed, i, self.bandit.f).keys()]
       if i not in seen_comps:
@@ -141,7 +127,6 @@ class COOP_algo():
           I_P = 1 - self.bandit.net_agents.nodes[u]['q'] * P
           prod *= I_P
         b = 1 - prod + epsilon
-        # print("b=",b, "|| l_i / b = ",l_i / b, "|| eta * l_i / b = ",l_i / b*self.eta)
         return l_i / b
     else:
       return 0.
@@ -151,30 +136,14 @@ class COOP_algo():
     arms = [a for a in range(len(self.bandit.net_agents.nodes))]
     for v, act in enumerate(self.bandit.activations):
       prob = probs[:,v]
-      # print("NEW LOSSES AVAILABLE: ", self.bandit.net_agents.nodes[v]['new_ls'])
-      # print("PREDICTION PROB: ", prob)
       self.bandit.net_agents.nodes[v]['m'] = prob
 
       if act == 1:
         [arms[v]] = np.random.choice(list(self.bandit.net_feed.nodes), 1, p = prob)
       else:
         arms[v] = None
-    # print("arms played:", arms)
     return arms
 
   def act(self):
     self.bandit.activate_players()
     self.bandit.play(self.sample())
-
-  def restart_algo(self, bandit, T):
-    self.bandit = bandit
-    f_var = self.bandit.arms()
-    s_var = self.bandit.net_agents.number_of_nodes()
-    self.W = np.ones((f_var, s_var))
-    self.P = np.ones((f_var, s_var))/f_var
-    self.T = T
-    self.buffer = deque()
-    self.alpha_feed = self.bandit.K if self.bandit.f == 0 else len(independent_set.maximum_independent_set(nx.power(self.bandit.net_feed, self.bandit.f)))
-    self.alpha_agents = self.bandit.A if self.bandit.n == 0 else len(independent_set.maximum_independent_set(nx.power(self.bandit.net_agents, self.bandit.n)))
-    self.Q = np.sum([self.bandit.net_agents.nodes[v]['q'] for v in self.bandit.net_agents.nodes])
-    self.eta = ev_eta_fixed(self.bandit.K, self.T, self.alpha_feed, self.alpha_agents, self.Q, self.bandit.f, self.bandit.n)
--- main.py
+++ main.py
@@ -12,7 +12,9 @@ def UB(x_list, d, K, alpha_feed, alpha_agents):
     l = [x for x in map(f, x_list)]
     return l
 
-def run_algo(ban, coop, T):
+def run_algo(arms_mean, A, K, n, f, p_ERa, p_ERf, T):
+    ban = bandit.Bandit(arms_mean, A, K, n, f, p_ERa, p_ERf, q)
+    coop = bandit.COOP_algo(ban, T)
     r = np.array([])
     for t in range(T):
         coop.act()
@@ -30,22 +32,12 @@ def run_algo(ban, coop, T):
             #     print("agent", v, "has neighbours:",[u for u in ban.net_agents.neighbors(v)],
             #     "T:", ban.net_agents.nodes[v]['T'], "r:",np.dot(ban.subopt, ban.net_agents.nodes[v]['T']), "Tsub:", ban.t - ban.net_agents.nodes[v]['T'][0])
         ########################################################################
-    return ban, coop, r/coop.Q
-
-def run_algo_COOPAndIndep(arms_mean, A, K, n, f, p_ERa, p_ERf, T):
-    ban = bandit.Bandit(arms_mean, A, K, n, f, p_ERa, p_ERf, q)
-    coop = bandit.COOP_algo(ban, T)
-    ban, coop, r = run_algo(ban, coop, T)
-    ban.restart_bandit()
-    ban.net_agents.remove_edges_from(ban.net_agents.edges())
-    coop.restart_algo(ban, T)
-    ban, coop, r_indepp = run_algo(ban, coop, T)
-    return r, r_indepp
+    return r
 
 n = 2; f = 2
 A = 20; K = 20
 q = 1/A
-T = 1000
+T = 10
 p_ERa = 0.1; p_ERf = 0.1
 seed_f =41; seed_a=43
 arms_mean = 1/2 * np.ones(K)
@@ -58,23 +50,22 @@ sample = 10
 # net_feed = nx.erdos_renyi_graph(K, p_ERf, seed = seed_f)
 # nx.draw_networkx(net_feed, pos=nx.spring_layout(net_feed), node_color ="red", alpha=0.9)
 # pp.axis ("off")
-# # net_agents = nx.erdos_renyi_graph(A, p_ERa, seed = seed_a)
-# # nx.draw_networkx(net_agents,  pos=nx.spring_layout(net_agents), node_color ="blue", alpha=0.9)
-# # pp.axis ("off")
-# # pp.show()
+# net_agents = nx.erdos_renyi_graph(A, p_ERa, seed = seed_a)
+# nx.draw_networkx(net_agents,  pos=nx.spring_layout(net_agents), node_color ="blue", alpha=0.9)
+# pp.axis ("off")
+# pp.show()
 # pp.savefig("out.pdf")
-
 
 if __name__ == "__main__":
     pool = mp.Pool() # mp.cpu_count()
     it = [(arms_mean, A, K, n, f, p_ERa, p_ERf, T) for s in range(sample)]
-    results = pool.starmap(run_algo_COOPAndIndep, it)
+    it_indep = [(arms_mean, A, K, 0, 0, 0., p_ERf, T) for s in range(sample)]
+    results = pool.starmap(run_algo, it+it_indep)
     pool.close()
     pool.join()
     results = np.array(results)
-    r = results[:,0]
-    r_indepp = results[:,1]
-
+    r = results[:sample,:]
+    r_indepp = results[sample:,:]
     means = np.mean(r, axis=0)
     errors = np.std(r, axis=0)
     means_indepp = np.mean(r_indepp, axis=0)
@@ -82,9 +73,9 @@ if __name__ == "__main__":
     x = [i for i in range(T)]
     # bound = UB(x, n+f, K, coop.alpha_feed, coop.alpha_agents)
     pp.figure(figsize=(10, 6))
-    pp.plot(x, means, label = "COOPalgo", color='blue')
+    pp.plot(x, means, label = r"EXP3-$\alpha^2$", color='blue')
     pp.fill_between(x, means - errors,means + errors, color='blue', alpha=0.2)
-    pp.plot(x, means_indepp, label = "independent", color='red')
+    pp.plot(x, means_indepp, label = "No Cooperation", color='red')
     pp.fill_between(x, means_indepp - errors_indepp,means_indepp + errors_indepp, color='red', alpha=0.2)
     pp.grid()
     # pp.plot(x, bound, label = "theory", color='black')
@@ -93,5 +84,7 @@ if __name__ == "__main__":
     tit_f = f"n={n}_f={f}_agents={A}_arms={K}_bias0={arms_mean[0]:.3}"
     tit_f += f"_q={q}_samples={sample}_ER_a={p_ERa}_ER_f={p_ERf}"
     pp.title(tit_g)
+    pp.xlabel("Number of Rounds")
+    pp.ylabel("Network Regret")
     pp.legend(loc = 2)
     pp.savefig(tit_f+'.pdf', dpi=600)
